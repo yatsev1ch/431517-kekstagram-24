2944367a8efadf8fc242817544ab135c03b08064
yatsev1ch <ayatsevdeveloper@gmail.com>
2021-11-08 00:15:18 +0300

    Добавлена работа с сетью

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
-import {getPosts} from './data.js';
+import {fetchPosts} from './network.js';
 import {generateThumbnailsFrom} from './thumbnails.js';
+import {showNotification} from './notifications.js';
 import './new-post.js';
 
-// const posts = getPosts(25);
-// generateThumbnailsFrom(posts);
-
-fetch('https://24.javascript.pages.academy/kekstagram/data')
-.then((response) => response.json()).then((data) => {
-  generateThumbnailsFrom(data);
-});
+fetchPosts(generateThumbnailsFrom, showNotification);
--- js/network.js
+++ js/network.js
@@ -1,6 +1,33 @@
-const requestPosts = (onSuccess, onError) => {
-  fetch('https://24.javascript.pages.academy/kekstagram/data')
-.then((response) => response.json()).then((data) => {
-  generateThumbnailsFrom(data);
-});
-}
+const loadNewPost = (formData, successHandler, errorHandler) => {
+  fetch(
+    'https://24.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body: formData,
+    },
+  ).then((response) => {
+    if (response.ok) {
+      successHandler('success');
+      return;
+    }
+    throw new Error();
+  }).catch(() => {
+    errorHandler('error');
+  });
+};
+
+
+const fetchPosts = (successHandler, errorHandler) => {
+  fetch('https://24.javascript.pages.academy/kekstagram/data').then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error();
+  }).then((posts) => {
+    successHandler(posts);
+  }).catch(() => {
+    errorHandler('load-error');
+  });
+};
+
+export {fetchPosts, loadNewPost};
