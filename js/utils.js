3f423a2417ac78c0109cbae4b579ab3299d35081
yatsev1ch <ayatsevdeveloper@gmail.com>
2021-11-08 00:23:50 +0300

    Удалено лишнее

--- js/utils.js
+++ js/utils.js
@@ -1,17 +1,3 @@
-const getRandomNumberFrom = (minValue, maxValue) => {
-  if (minValue < 0 || (maxValue <= minValue)) {
-    return 'Введите корректные значения диапозона';
-  }
-  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
-};
-
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
-
-const getRandomElementFrom = (array) => {
-  const index = getRandomNumberFrom(0, array.length - 1);
-  return array[index];
-};
-
 const checkForActiveElementsIn = (elements) => {
   for (const element of elements) {
     if (document.activeElement === element) {
@@ -35,33 +21,11 @@ const checkArrayForDuplicate = (array) => {
 
 const isEscape = (evt) => evt.key === 'esc' || evt.key === 'Escape';
 
-
-const getUniqueId = (minValue, maxValue, usedIds) => {
-  let id = getRandomNumberFrom(minValue, maxValue);
-  if (usedIds.length !== 0) {
-    while (usedIds.includes(id)) {
-      id = getRandomNumberFrom(minValue, maxValue);
-    }
-  }
-  usedIds.push(id);
-  return id;
-};
-
-const createMessageFrom = (arrayOfStrings, numberOfStrings) => {
-  const usedStrings = [];
-  for (let iterator = 1; iterator <= numberOfStrings; iterator++) {
-    const string = getRandomElementFrom(arrayOfStrings);
-    usedStrings.push(string);
-  }
-
-  return usedStrings.join(' ');
-};
-
 const resetRadioButtonsTo = (value, radioButtons) => {
   radioButtons.forEach((button) => {
     button.checked = button.value === value;
   });
 };
 
-export {getRandomNumberFrom, checkStringLength, getRandomElementFrom, getUniqueId, createMessageFrom, isEscape, checkArrayForDuplicate, checkForActiveElementsIn, resetRadioButtonsTo};
+export {isEscape, checkArrayForDuplicate, checkForActiveElementsIn, resetRadioButtonsTo};
 
